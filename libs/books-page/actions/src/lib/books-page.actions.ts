a05f860fa20816ed9d24f8167b509452ebc2fd5f
Juan Ruiz <rmjuanruiz@gmail.com>
2023-05-11 17:50:18 -0500

    Add ngrx in the application

--- libs/books-page/actions/src/lib/books-page.actions.ts
+++ libs/books-page/actions/src/lib/books-page.actions.ts
@@ -1,2 +1,36 @@
 import { createAction, props } from '@ngrx/store';
 import { BookRequiredProps } from '@book-co/shared-models';
+
+/* [User on Books Page] Enter
+ * [User on Books Page] Select a Book
+ * [User on Books Page] Clear Selected Book
+ * [User on Books Page] Create Book
+ * [User on Books Page] Update Book
+ * [User on Books Page] Delete Book
+ */
+
+export const enter = createAction('[Books Page] Enter');
+
+export const selectBook = createAction(
+  '[Books Page] Select a Book',
+  props<{ bookId: string }>()
+);
+
+export const clearSelectedBook = createAction(
+  '[Books Page] Clear Selected Book'
+);
+
+export const createBook = createAction(
+  '[Books Page] Create Book',
+  props<{ book: BookRequiredProps }>()
+);
+
+export const updateBook = createAction(
+  '[Books Page] Update Book',
+  props<{ bookId: string; changes: BookRequiredProps }>()
+);
+
+export const deleteBook = createAction(
+  '[Books Page] Delete Book',
+  props<{ bookId: string }>()
+);
--- libs/books-page/src/lib/book-api.effects.ts
+++ libs/books-page/src/lib/book-api.effects.ts
@@ -7,4 +7,55 @@ import { BooksPageActions, BooksApiActions } from '@book-co/books-page/actions';
 @Injectable()
 export class BooksApiEffects {
   constructor(private booksService: BooksService, private actions$: Actions) {}
+
+  // mergeMap => deleting items // It is going to do all requests in a different order
+  // contactMap => Updating or creating items // Its going to do all requets in the order that was requested, one by one
+  // exhaustMap => non-parameterized queries // If there is already a request, its going to discard the last one, we are all trying to get the same information (getAllBooks)
+  // switchMap => Parametized queries // Its going to discard last request and return the new one
+
+  loadBooks$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(BooksPageActions.enter),
+      exhaustMap(() => {
+        return this.booksService
+          .all()
+          .pipe(map((books) => BooksApiActions.booksLoaded({ books })));
+      })
+    );
+  });
+
+  createBook$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(BooksPageActions.createBook),
+      concatMap((action) => {
+        return this.booksService
+          .create(action.book)
+          .pipe(map((book) => BooksApiActions.bookCreated({ book })));
+      })
+    );
+  });
+
+  updateBook$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(BooksPageActions.updateBook),
+      concatMap((action) => {
+        return this.booksService
+          .update(action.bookId, action.changes)
+          .pipe(map((book) => BooksApiActions.bookUpdated({ book })));
+      })
+    );
+  });
+
+  deleteBook$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(BooksPageActions.deleteBook),
+      mergeMap((action) => {
+        return this.booksService
+          .delete(action.bookId)
+          .pipe(
+            map(() => BooksApiActions.bookDeleted({ bookId: action.bookId }))
+          );
+      })
+    );
+  });
 }
